86e27039814857fc2025ed2b91aed0bafc2bbc2a
Covid 19 <covid@IFRSL.redelocal>
2022-08-09 17:28:41 -0300

    Início da implementação do sistema (09/08)

---
+++ ctrl.php
@@ -0,0 +1,4 @@
+<?php
+    $ip = isset($_POST["ip"]) ? $_POST["ip"] : "";
+    $mask = isset($_POST["mask"]) ? $_POST["ip"] : "";
+?>
---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Máscara de Rede</title>
+</head>
+<body>
+    <br>
+    <form action="ctrl.php" method="post">
+        Insira um Endereço IP: <input type="text" name="ip"><br>
+        <br>
+        Insira um Endereço de Máscara de Rede: <input type="text" name="mask"><br>
+        <br>
+        <button type="submit">Enviar dados</button>
+    </form>
+</body>
+</html>
+
+<!--
+    Ideia:
+    - Ter um campo para cada quadrante dos endereços
+-->
