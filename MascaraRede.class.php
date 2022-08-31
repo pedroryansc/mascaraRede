4debdff20ff859713ef290670e853f44ad414cac
pedroryansc <pedroryangamer@gmail.com>
2022-08-30 22:55:24 -0300

    Conclusão e implementação com Classe

---
+++ MascaraRede.class.php
@@ -0,0 +1,119 @@
+<?php
+    class MascaraRede{
+        private $ip;
+        private $mask;
+        private $rede;
+        private $primeiroIp;
+        private $ultimoIp;
+        private $broadcast;
+        public function __construct($ip, $mask){
+            $this->setIp($ip);
+            $this->setMask($mask);
+            $this->setRede();
+            $this->setPrimeiroIp();
+            $this->setUltimoIp();
+            $this->setBroadcast();
+        }
+
+        public function setIp($ip){
+            if($ip <> "")
+                $this->ip = $ip;
+            else
+                throw new Exception("Insira um endereço IP, por favor");
+        }
+        public function setMask($mask){
+            if($mask <> "")
+                $this->mask = $mask;
+            else
+                throw new Exception("Insira um endereço de máscara de sub-rede, por favor");
+        }
+        public function setRede(){
+            $maskAndBits = $this->separaMaskBits();
+            $this->rede = long2ip((ip2long($this->getIp())) & (ip2long($maskAndBits[0])));
+        }
+        public function setPrimeiroIp(){
+            $this->primeiroIp = long2ip(ip2long($this->getRede()) | 1);
+        }
+        public function setUltimoIp(){
+            $maskAndBits = $this->separaMaskBits();
+            $this->ultimoIp = long2ip(ip2long($this->getRede()) | ((~(ip2long($maskAndBits[0]))) - 1));
+        }
+        public function setBroadcast(){
+            $maskAndBits = $this->separaMaskBits();
+            $this->broadcast = long2ip(ip2long($this->getRede()) | (~(ip2long($maskAndBits[0]))));
+        }
+
+        public function getIp(){ return $this->ip; }
+        public function getMask(){ return $this->mask; }
+        public function getRede(){ return $this->rede; }
+        public function getPrimeiroIp(){ return $this->primeiroIp; }
+        public function getUltimoIp(){ return $this->ultimoIp; }
+        public function getBroadcast(){ return $this->broadcast; }
+
+        public function separaMaskBits(){
+            return explode("/", $this->getMask());
+        }
+
+        public function ip2bin($end){ 
+            return base_convert(ip2long($end), 10, 2);
+        }
+
+        public function __toString(){
+            return "<p><b>Decimal:</b></p>
+            <table border='1'>
+                <tr>
+                    <th>Endereço IP</th>
+                    <td>".$this->getIp()."</td>
+                </tr>
+                <tr>
+                    <th>Máscara de sub-rede</th>
+                    <td>".$this->getMask()."</td>
+                </tr>
+                <tr>
+                    <th>Endereço de rede</th>
+                    <td>".$this->getRede()."</td>
+                </tr>
+                <tr>
+                    <th>Primeiro IP útil</th>
+                    <td>".$this->getPrimeiroIp()."</td>
+                </tr>
+                <tr>
+                    <th>Último IP útil</th>
+                    <td>".$this->getUltimoIp()."</td>
+                </tr>
+                <tr>
+                    <th>Endereço de Broadcast</th>
+                    <td>".$this->getBroadcast()."</td>
+                </tr>
+            </table>
+            <br>
+            <p><b>Binário:</b></p>
+            <table border='1'>
+                <tr>
+                    <th>Endereço IP</th>
+                    <td>".$this->ip2bin($this->getIp())."</td>
+                </tr>
+                <tr>
+                    <th>Máscara de sub-rede</th>
+                    <td>".$this->ip2bin($this->separaMaskBits($this->getMask())[0])."/".$this->separaMaskBits($this->getMask())[1]."</td>
+                </tr>
+                <tr>
+                    <th>Endereço de rede</th>
+                    <td>".$this->ip2bin($this->getRede())."</td>
+                </tr>
+                <tr>
+                    <th>Primeiro IP útil</th>
+                    <td>".$this->ip2bin($this->getPrimeiroIp())."</td>
+                </tr>
+                <tr>
+                    <th>Último IP útil</th>
+                    <td>".$this->ip2bin($this->getUltimoIp())."</td>
+                </tr>
+                <tr>
+                    <th>Endereço de Broadcast</th>
+                    <td>".$this->ip2bin($this->getBroadcast())."</td>
+                </tr>
+            </table>";
+        }
+    }
+?>
--- index.php
+++ index.php
@@ -1,19 +1,10 @@
 <!DOCTYPE html>
 <?php
-    // Endereço IP
     $ip = isset($_POST["ip"]) ? $_POST["ip"] : 0;
-
-    // Quantidade de bits da Máscara
     $mask = isset($_POST["mask"]) ? $_POST["mask"] : 0;
-
-    // Conversão de Endereço IP para binário
-    function ip2bin($ip){ 
-        return base_convert(ip2long($ip),10,2);
-    }
-
-    // Conversão de binário para Endereço IP
+    
     function bin2ip($bin){ 
-        return long2ip(base_convert($bin,2,10));
+        return long2ip(base_convert($bin, 2, 10));
     }
 ?>
 <html lang="pt-br">
@@ -21,15 +12,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Máscara de Rede</title>
+    <title>Calculadora de endereços de rede</title>
+    <style>
+        td{
+            font-family: "Courier New";
+        }
+    </style>
 </head>
 <body>
     <br>
     <form method="post">
-        Insira um Endereço IP:
-        <input type="text" name="ip">
-        <br><br>
-        Escolha um Endereço de Máscara de Sub-rede: <select name="mask">
+        Insira um endereço IP: <input type="text" name="ip" value="<?php if(isset($_POST["ip"])) echo $ip; ?>"><br>
+        <br>
+        Escolha um endereço de Máscara de Sub-rede: <select name="mask">
             <?php   
                 for($i = 1; $i <= 30; $i ++){
                     $maskOptionBin = "";
@@ -40,64 +35,22 @@
                             $maskOptionBin .= "0";
                     }
                     $maskOptionDec = bin2ip($maskOptionBin);
-                    echo "<option value=".$maskOptionDec."/".$i.">".$maskOptionDec."/".$i."</option>";
+                    $maskOption = $maskOptionDec."/".$i;
+            ?>
+                <option value="<?php echo $maskOption; ?>" <?php if($maskOption == $mask) echo "selected"; ?>> <?php echo $maskOption; ?> </option>
+            <?php
                 }
             ?>
-        </select>
-        <br><br>
+        </select><br>
+        <br>
         <button type="submit">Enviar dados</button>
     </form>
     <br>
     <?php
         if(isset($_POST["mask"])){
-            $maskAndBits = explode("/", $mask);
-
-            // Endereços inseridos
-            echo "$ip e $mask <br>";
-
-            // Endereço IP em binário
-            $ipbin = ip2bin($ip);
-
-            // Endereço de Rede
-            $rede = long2ip((ip2long($ip)) & (ip2long($maskAndBits[0])));
-
-            // Primeiro IP útil
-            $primeiroIp = long2ip(ip2long($rede) | 1);
-
-            // Último IP útil
-            $ultimoIp = long2ip(ip2long($rede) | ((~(ip2long($maskAndBits[0]))) - 1));
-
-            // Endereço de Broadcast
-            $broadcast = long2ip(ip2long($rede) | (~(ip2long($maskAndBits[0]))));
-    ?>
-    <p>Decimal:</p>
-    <table>
-        <tr>
-            <th>Endereço IP</th>
-            <td><?php echo $ip; ?></td>
-        </tr>
-        <tr>
-            <th>Máscara de sub-rede</th>
-            <td><?php echo $mask; ?></td>
-        </tr>
-        <tr>
-            <th>Endereço de rede</th>
-            <td><?php echo $rede; ?></td>
-        </tr>
-        <tr>
-            <th>Primeiro IP útil</th>
-            <td><?php echo $primeiroIp; ?></td>
-        </tr>
-        <tr>
-            <th>Último IP útil</th>
-            <td><?php echo $ultimoIp; ?></td>
-        </tr>
-        <tr>
-            <th>Endereço de Broadcast</th>
-            <td><?php echo $broadcast; ?></td>
-        </tr>
-    </table>
-    <?php
+            require_once("MascaraRede.class.php");
+            $maskRede = new MascaraRede($ip, $mask);
+            echo $maskRede;
         }
     ?>
 </body>
